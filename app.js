5c99484c2bc2e89b4dc1233cc90fff73867d2109
Priyam Chakraborty <35304050+cpriyam16@users.noreply.github.com>
2024-05-31 21:13:41 +0530

    Namaste React : Inception

---
+++ app.js
@@ -0,0 +1,12 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading", className: "h1" },
+  "Namaste React!"
+);
+const heading2 = React.createElement("h2", { id: "heading2" }, "Sub Heading");
+const child = React.createElement("div", { id: "child" }, [heading, heading2]);
+const parent = React.createElement("div", { id: "parent" }, child);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
