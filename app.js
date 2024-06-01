21ce362ffea71dce216b0cd1867367a7cb32ba65
Priyam <cpriyam16@gmail.com>
2024-06-01 19:21:59 +0530

    end of 2 ep

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+import React from "react"
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement(
   "h1",
   { id: "heading", className: "h1" },
   "Namaste React!"
 );
+
 const heading2 = React.createElement("h2", { id: "heading2" }, "Sub Heading");
 const child = React.createElement("div", { id: "child" }, [heading, heading2]);
 const parent = React.createElement("div", { id: "parent" }, child);
