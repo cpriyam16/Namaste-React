e8c72047228dde1f13c43525ef25de6529ba55b2
Priyam <cpriyam16@gmail.com>
2025-04-17 00:37:50 +0530

    Day1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React, { Suspense, lazy } from "react";
-import ReactDOM from "react-dom/client";
-
-const AppLayout = () => {
-    return (
-        <div className="app"></div>
-    );
-};
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(<RouterProvider router={appRouter} />);
+const heading = React.createElement(
+    'div',
+    { id: 'parent', class: 'parent' },
+    React.createElement('div', { id: 'child', class: 'child' }, [
+        React.createElement(
+            'h1',
+            { id: 'heading', class: 'heading' },
+            'Hello world React',
+        ),
+        React.createElement(
+            'h2',
+            { id: 'heading2', class: 'heading2' },
+            'Hello world React 2',
+        ),
+    ]),
+);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
